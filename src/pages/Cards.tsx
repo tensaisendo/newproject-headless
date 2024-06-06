9afbf35b6b19fe27e32668a0b2ea586ac66908d9
tensaisendo <tensai.sendo@gmail.com>
2024-06-06 22:04:08 +0200

    test single 2

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,75 +1,66 @@
-// import { gql, useQuery } from "@apollo/client";
-// import PageLayout from "../container/PageLayout";
+import { gql, useQuery } from "@apollo/client";
+import PageLayout from "../container/PageLayout";
 
-// const GET_CARDS = gql`
-//   query getCards {
-//     cards {
-//       nodes {
-//         title
-//         slug
-//         cardsFields {
-//           cardTitle
-//           color
-//           counter
-//           effect
-//           life
-//           power
-//           rarity
-//           type
-//           image {
-//             node {
-//               mediaDetails {
-//                 file
-//                 height
-//                 width
-//               }
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+const GET_CARDS = gql`
+  query getCards {
+    cards {
+      nodes {
+        title
+        slug
+        cardsFields {
+          cardTitle
+          color
+          counter
+          effect
+          life
+          power
+          rarity
+          type
+          image {
+            node {
+              mediaDetails {
+                file
+                height
+                width
+              }
+              slug
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-// const Cards = () => {
-//   const { loading, error, data } = useQuery(GET_CARDS);
+const Cards = () => {
+  const { loading, error, data } = useQuery(GET_CARDS);
 
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error! {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error! {error.message}</p>;
 
-//   return (
-//     <PageLayout>
-//       <h1>Card List</h1>
-//       <ul>
-//         {data.cards.nodes.map((card: any) => (
-//           <li key={card.slug}>
-//             <h2>{card.title}</h2>
-//             <p>Slug: {card.slug}</p>
-//             <p>Card Title: {card.cardsFields.cardTitle}</p>
-//             <p>Color: {card.cardsFields.color.join(", ")}</p>
-//             <p>Counter: {card.cardsFields.counter}</p>
-//             <p>Effect: {card.cardsFields.effect}</p>
-//             <p>Life: {card.cardsFields.life}</p>
-//             <p>Power: {card.cardsFields.power}</p>
-//             <p>Rarity: {card.cardsFields.rarity.join(", ")}</p>
-//             <p>Type: {card.cardsFields.type.join(", ")}</p>
-//             {card.cardsFields.image && (
-//               <img
-//                 src={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-content/uploads/${card.cardsFields.image.node.mediaDetails.file}`}
-//                 alt={card.cardsFields.image.node.slug}
-//                 height={card.cardsFields.image.node.mediaDetails.height}
-//                 width={card.cardsFields.image.node.mediaDetails.width}
-//               />
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </PageLayout>
-//   );
-// };
+  return (
+    <PageLayout>
+      <h1>Card List</h1>
+      <ul>
+        {data.cards.nodes.map((card: any) => (
+          <li key={card.slug}>
+            <h2>{card.title}</h2>          
+            {card.cardsFields.image && (
+              <img
+                src={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-content/uploads/${card.cardsFields.image.node.mediaDetails.file}`}
+                alt={card.cardsFields.image.node.slug}
+                height={card.cardsFields.image.node.mediaDetails.height}
+                width={card.cardsFields.image.node.mediaDetails.width}
+              />
+            )}
+          </li>
+        ))}
+      </ul>
+    </PageLayout>
+  );
+};
 
-// export default Cards;
+export default Cards;
 
 import React, { FC } from "react";
 import NcImage from "@/components/NcImage/NcImage";
@@ -142,3 +133,62 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
 };
 
 export default SingleType1;
+
+import React, { FC } from "react";
+import NcImage from "@/components/NcImage/NcImage";
+import { getPostDataFromPostFragment } from "@/utils/getPostDataFromPostFragment";
+import SingleHeader from "../SingleHeader";
+import { SingleType1Props } from "../single/single";
+interface Props extends SingleType1Props {}
+
+const SingleType2: FC<Props> = ({ post }) => {
+  //
+  const {
+    title,
+    content,
+    date,
+    author,
+    databaseId,
+    excerpt,
+    featuredImage,
+    ncPostMetaData,
+  } = getPostDataFromPostFragment(post || {});
+  //
+
+  const imgWidth = featuredImage?.mediaDetails?.width || 1000;
+  const imgHeight = featuredImage?.mediaDetails?.height || 750;
+  return (
+    <div className={`pt-8 lg:pt-16`}>
+      {/* SINGLE HEADER */}
+      <header className="container rounded-xl">
+        <div className="max-w-screen-md mx-auto">
+          <SingleHeader post={{ ...post }} hiddenDesc />
+          {!featuredImage?.sourceUrl && (
+            <div className="my-5 border-b border-neutral-200 dark:border-neutral-800 "></div>
+          )}
+        </div>
+      </header>
+
+      {/* FEATURED IMAGE */}
+      {featuredImage?.sourceUrl && (
+        <NcImage
+          alt={title}
+          containerClassName="container my-10 sm:my-12"
+          className={`rounded-xl mx-auto ${
+            imgWidth <= 768 && ncPostMetaData?.showRightSidebar
+              ? "w-full max-w-screen-md"
+              : ""
+          }`}
+          src={featuredImage?.sourceUrl || ""}
+          width={imgWidth}
+          height={imgHeight}
+          sizes={"(max-width: 1024px) 100vw, 1280px"}
+          enableDefaultPlaceholder
+          priority
+        />
+      )}
+    </div>
+  );
+};
+
+export default SingleType2;
