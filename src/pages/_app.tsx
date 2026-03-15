006b98204ccff184af76e98afaa308a4eea9142c
tensaisendo <tensai.sendo@gmail.com>
2026-03-15 11:21:03 +0100

    Ajout du Favicon pour le front

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,8 @@ import themeJson from "../../theme.json";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '@/lib/apolloClient';
+import Head from "next/head";
+
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,6 +30,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png"/>
+      </Head>
+
       <GoogleAnalytics trackPageViews />
       <ApolloProvider client={apolloClient}>
         <FaustProvider pageProps={pageProps}>
